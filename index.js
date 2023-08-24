5f5703b014a91b5c6c5dc092e249cd030ad7ab07
giovgio90 <giovgio90@gmail.com>
2023-08-24 22:11:59 +0200

    update

--- index.js
+++ index.js
@@ -1,35 +1,79 @@
-const apiKey = "CuDiCh3GB1AagMLedkRJ8qRV9EeNKBcBroHDXmpN2vdvTYcLIXjqrb5I";
-const loadImagesBtn = document.getElementById("loadImagesBtn");
-const loadSecondaryImagesBtn = document.getElementById("loadSecondaryImagesBtn");
-const card = document.querySelectorAll(".card");
+const imageCard = (image) => `<div class="col mb-4">
+                          <div class="card mb-4 shadow-sm h-100">
+                          <img src=${image.src.large} class="card-img-top" alt=${image.alt} style="height: 200px; object-fit: cover; cursor: pointer" onclick="goToDetails(${image.id})">
+                            <div class="card-body d-flex flex-column">
+                              <h5 class="card-title" onclick="goToDetails(${image.id})" style="cursor: pointer">${image.alt}</h5>
+                                <p class="card-text mt-auto">
+                                  <a href=${image.photographer_url} target="_blank">
+                                      ${image.photographer}
+                                  </a>
+                                </p>
+                                <div
+                                  class="d-flex justify-content-between align-items-center"
+                                >
+                                  <div class="btn-group">
+                                    <button
+                                      type="button"
+                                      class="btn btn-sm btn-outline-secondary"
+                                      onclick="modalLogic(event)"
+                                    >
+                                      View
+                                    </button>
+                                    <button
+                                      type="button"
+                                      class="btn btn-sm btn-outline-secondary"
+                                        onclick="hideMe(event)">
+                                      Hide
+                                    </button>
+                                  </div>
+                                  <small class="text-muted">${image.id}</small>
+                              </div>
+                            </div>
+                          </div>
+                        </div>`;
 
-const headers = {
-  Authorization: apiKey,
+const hideMe = (event) => {
+  event.currentTarget.closest(".col").remove();
 };
 
-loadImagesBtn.addEventListener("click", () => {
-  const apiUrl = "https://api.pexels.com/v1/search?query=people";
+let loadImages = async (query) => {
+  const apiKey = "CuDiCh3GB1AagMLedkRJ8qRV9EeNKBcBroHDXmpN2vdvTYcLIXjqrb5I";
 
-  fetch(apiUrl, { headers })
-    .then((response) => response.json())
-    .then((data) => {
-      const photos = data.photos;
-      const cardElements = document.querySelectorAll(".card");
+  const resp = await fetch("https://api.pexels.com/v1/search?query=" + query, {
+    headers: {
+      Authorization: apiKey,
+    },
+  });
+  const body = await resp.json();
 
-      photos.forEach((photo, index) => {
-        if (index < cardElements.length) {
-          const card = cardElements[index];
-          const imageElement = card.querySelector(".card-img-top");
-          const titleElement = card.querySelector(".card-title");
-          const textElement = card.querySelector(".card-text");
+  const grid = document.getElementById("myGrid");
 
-          imageElement.src = photo.src.medium;
-          titleElement.textContent = photo.alt;
-          textElement.textContent = `Photographer: ${photo.photographer}`;
-        }
-      });
-    })
-    .catch((error) => {
-      console.error("Error occurred:", error);
-    });
-});
+  grid.innerHTML = "";
+  body.photos.forEach((photo) => {
+    grid.innerHTML += imageCard(photo);
+  });
+};
+
+let searchQuery;
+
+const handleSearchQuery = (ev) => {
+  searchQuery = ev.target.value.toLowerCase();
+};
+
+const searchImages = () => {
+  loadImages(searchQuery);
+};
+
+window.onload = () => {
+  const loadImagesBtn = document.querySelector(".jumbotron a.btn:first-of-type");
+  loadImagesBtn.onclick = () => loadImages("nature");
+
+  const loadSecondaryImagesBtn = document.querySelector(".jumbotron a.btn:nth-of-type(2)");
+  loadSecondaryImagesBtn.onclick = () => loadImages("ocean");
+
+  const searchInput = document.querySelector(".jumbotron .form-control");
+  searchInput.oninput = (event) => handleSearchQuery(event);
+
+  const searchBtn = document.querySelector(".jumbotron button.btn");
+  searchBtn.onclick = () => searchImages();
+};
